ace00f893cc0580eac4bb75e5ead14f517d83186
Shiiou <112298761+shiiou@users.noreply.github.com>
2025-02-25 23:45:34 +0100

    Delete packps1 directory

--- packps1/Lancer l'installation du pack 'Shiiou V2'.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -UseBasicParsing 'https://shiiou.github.io/packps1/shiiouv2.ps1' | Invoke-Expression}"
-pause
-exit /b
